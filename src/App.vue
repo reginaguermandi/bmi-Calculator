fe1b6a20ecff643bff7c9c10774f1c1041d8c84f
reginaguermandi <reginaguermandi@gmail.com>
2022-08-16 14:44:12 -0300

    add new keys and body type function

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,11 @@
       <input id="height" type="number" v-model="height">
     </div>
 
-    <button class="btn-calculation" @click="handleCalculateBtn">Calcular</button>
+    <button class="btn-calculation" @click="calculateImc(); newBodyType();">Calcular</button>
 
     <div class="result">
-      <p class="imc-result"></p>
-      <p class="body-type"></p>
+      <p class="imc-result">{{ imc }}</p>
+      <p class="body-type">{{ bodyType }}</p>
     </div>
   </div>
 </template>
@@ -28,18 +28,23 @@ export default {
   data() {
     return {
       weight: 0,
-      height: 0
-    }
-  },
-  computed: {
-    calculateImc: function () {
-      let imc = (this.weight / (this.height * this.height)).toFixed(2);
-      return imc
+      height: 0,
+      imc: 0,
+      bodyType: 'Insira seus dados'
     }
   },
   methods: {
-    handleCalculateBtn: function () {
-      console.log(this.calculateImc)
+    calculateImc: function () {
+      return this.imc = (this.weight / (this.height * this.height)).toFixed(2);
+    },
+    newBodyType: function () {
+      if (this.imc >= 16 && this.imc <= 16.9 || this.imc < 16) return this.bodyType = `Muito abaixo do peso.`
+      if (this.imc >= 17 && this.imc <= 18.4) return this.bodyType = `Abaixo do peso.`
+      if (this.imc >= 18.5 && this.imc <= 24.9) return this.bodyType = `Peso normal.`
+      if (this.imc >= 25 && this.imc <= 29.9) return this.bodyType = `Acima do peso.`
+      if (this.imc >= 30 && this.imc <= 34.9) return this.bodyType = `Obesidade grau I.`
+      if (this.imc >= 35 && this.imc <= 40) return this.bodyType = `Obesidade grau II.`
+      if (this.imc > 40) return this.bodyType = `Obesidade grau III.`
     }
   }
 }
